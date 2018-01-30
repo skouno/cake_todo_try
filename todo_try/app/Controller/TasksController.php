910e4ea4bc908abe576bc277b9bccb0d48aaf2b3
Shunsuke KOUNO <13nm664t@gmail.com>
2018-01-30 17:37:25 +0900

    モデルの呼び出し

--- todo_try/app/Controller/TasksController.php
+++ todo_try/app/Controller/TasksController.php
@@ -4,8 +4,14 @@ class TasksController extends AppController {
     public $scaffold;
 
     public function index() {
-        // 空のデータをビューへ渡す
-        $tasks_data = array();
+        // データをモデルから取得してビューへ渡す
+        $options = array(
+            'conditions' => array(
+                'Task.status' => 0
+            )
+        );
+
+        $tasks_data = $this->Task->find('all', $options);
         $this->set('tasks_data',$tasks_data);
 
         // app/View/Tasks/index.ctpを表示
@@ -13,11 +19,11 @@ class TasksController extends AppController {
     }
 
     public function done() {
-        // URLの末尾からタスクのIDを取得してメッセージを作成
-        $msg = sprintf(
-            'タスク %s を完了しました。',
-            $this->request->pass[0]
-        );
+        // URLの末尾からタスクのIDを取得してデータを更新
+        $id = $this->request->pass[0];
+        $this->Task->id = $id;
+        $this->Task->saveField('status',1);
+        $msg = sprintf('タスク %s を完了しました。',$id);
 
         //メッセージを表示してリダイレクト
         $this->flash($msg,'/Tasks/index');
